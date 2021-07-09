9c1d6aeaf0447c488d63e796562387d1e71ae087
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-07-09 13:28:30 +0530

    Update CitusTrainingContext.cs

--- demoapp/Models/CitusTrainingContext.cs
+++ demoapp/Models/CitusTrainingContext.cs
@@ -23,11 +23,7 @@ namespace demoapp.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-//                optionsBuilder.UseSqlServer("Server=tcp:trgdb001.database.windows.net,1433;Initial Catalog=CitusTraining;Persist Security Info=False;User ID=MaheshAdmin;Password=P@ssw0rd_;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-//            }
+//           
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
